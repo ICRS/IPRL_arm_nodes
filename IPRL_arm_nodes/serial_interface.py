f76333bf83145b6cdb61585e855046034297d787
Meigan Teo <meigan.teo21@imperial.ac.uk>
2025-05-28 17:53:31 +0100

    Add serial publisher

--- IPRL_arm_nodes/serial_interface.py
+++ IPRL_arm_nodes/serial_interface.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 import serial
 
-from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
 
 
 class SerialInterface(Node):
@@ -10,34 +10,36 @@ class SerialInterface(Node):
     def __init__(self):
         super().__init__('serial_interface')
         self.subscription = self.create_subscription(
-            String,
+            Float64MultiArray,
             'joint_angles',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
         self.prevAngles = []
+        self.safety_value = 20
 
         # SERIAL CONNECTION
-        self.ser = serial.Serial('/dev/ttyUSB0')
+        self.baud_rate = 115200
+        self.ser = serial.Serial('/dev/ttyUSB0', self.baud_rate)
 
     def listener_callback(self, msg):
         # This implementation sends only the angle differences
         angles = msg.data # Shoulder, Elbow, Wrist, Base, Roll
+        angle_diffs = [0,0,0,0,0]
         if (self.prevAngles != []):
-            angle_diff = []
-            for i in range (0, len(msg.data)):
-                angle_diff[i] = msg.data[i] - self.prevAngles[i]
-        self.prevAngles = msg.data
+            for i in range (0, len(angles)):
+                angle_diffs[i] = angles[i] - self.prevAngles[i]
+        self.prevAngles = angles
 
         # Send angle differences over serial
-        for i in range (0, len(angle_diff)):
-            if (angle_diff[i] > 0.001):
-                message = "<MOTOR:" + str(i) + "|" + str(angle_diff[i]) + ">"
-                self.ser.write(message)
-        
-        self.get_logger().info('Sent angle diffs: %s', str(angle_diff))
+        for i in range (0, len(angle_diffs)):
+            if (abs(angle_diffs[i]) > 0.001)&(abs(angle_diffs[i]) < self.safety_value):
+                message = "<MOTOR:" + str(i) + "|" + str(angle_diffs[i]) + ">"
+                self.ser.write(message.encode("utf-8"))
 
+                self.get_logger().info('Sent message: %s' % message)
+        
 
 def main(args=None):
     rclpy.init(args=args)
