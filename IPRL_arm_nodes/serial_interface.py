9f8f6e3525ca1382b084a4a5814bc2ef463d4771
ben-linux <bens15351@gmail.com>
2025-08-27 16:51:36 +0100

    Added device ID checking

---
+++ IPRL_arm_nodes/serial_interface.py
@@ -0,0 +1,146 @@
+import rclpy
+from rclpy.node import Node
+import serial
+import glob
+import time
+import re
+
+from std_msgs.msg import Float64MultiArray
+
+
+class SerialInterface(Node):
+
+    PATTERN_PONG      = re.compile(r"<PONG:(\d+)>")
+
+    def __init__(self):
+        super().__init__('serial_interface')
+        self.subscription = self.create_subscription(
+            Float64MultiArray,
+            'joint_angles',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+        self.prevAngles = []
+        self.safety_value = 20
+
+        # Setup serial
+        ports = sorted(glob.glob('/dev/ttyUSB*'))
+        if not ports:
+            self.get_logger().error("No /dev/ttyACM* devices found.")
+            raise RuntimeError("No ACM serial device found.")
+        self.ser = None
+
+        # Get the port
+        connected = False
+        self.baud_rate = 115200
+        while not connected:
+            for port in ports:
+                try:
+                    self.get_logger().info(f"Trying serial port: {port}")
+                    self.ser = serial.Serial(port, self.baud_rate, timeout=1)
+                    time.sleep(2)  # give device time to reset if needed
+                    self.get_logger().info(f"Connected to {port}")
+                except serial.SerialException as e:
+                    self.get_logger().warn(f"Failed to open {port}: {e}")
+
+                # Check the serial port is okay
+                if self.ser is None:
+                    self.get_logger().info("Couldn't get serial port")
+                    time.sleep(1)
+                    continue
+
+                # Ping the device to check it responds and has the correct ID
+                connected = self.serial_ping()
+
+                # Invalid device: close the connection and try the next device
+                if not connected:
+                    self.ser.close()
+                    continue
+
+                # Valid device: move on
+                else:
+                    break
+
+    def serial_ping(self) -> bool:
+
+        buffer = ""
+        start_time = time.time()
+        pong_received = False
+        while not pong_received:
+
+            # Timeout
+            if (time.time() > (start_time + 3.0)) and not pong_received:
+                break
+
+            # Send PING
+            out = "<PING:1>\n"
+            out_bytes = bytes(out.encode("utf-8"))
+            try:
+                self.ser.write(out_bytes)
+                self.get_logger().info("Sent PING")
+            except serial.serialutil.SerialException:
+                continue
+
+            time.sleep(0.2)
+
+            # Wait for PONG
+            if self.ser.in_waiting:
+                buffer += self.ser.read(self.ser.in_waiting).decode(errors='ignore')
+                while '\n' in buffer:
+                    line, buffer = buffer.split('\n', 1)
+                    line = line.strip()
+
+                    # Check if the message is a PONG
+                    m = SerialInterface.PATTERN_PONG.match(line)
+                    if m is not None:
+
+                        device_id = int(m.group(1))
+
+                        # PONG contained invalid ID
+                        if device_id != 50:
+                            self.get_logger().info(f"Received PONG, incorrect ID: '{device_id}'")
+                            return pong_received
+
+                        # PONG containted correct ID
+                        else:
+                            pong_received = True
+                            self.get_logger().info(f"Received PONG, correct ID: '{device_id}'")
+                            return pong_received
+
+        return pong_received
+    
+    def listener_callback(self, msg):
+        # This implementation sends only the angle differences
+        angles = msg.data # Shoulder, Elbow, Wrist, Base, Roll
+        angle_diffs = [0,0,0,0,0]
+        if (self.prevAngles != []):
+            for i in range (0, len(angles)):
+                angle_diffs[i] = angles[i] - self.prevAngles[i]
+        self.prevAngles = angles
+
+        # Send angle differences over serial
+        for i in range (0, len(angle_diffs)):
+            if (abs(angle_diffs[i]) > 0.001)&(abs(angle_diffs[i]) < self.safety_value):
+                message = "<MOTOR_" + str(i) + ":" + str(angle_diffs[i]) + ">\n"
+                self.ser.write(message.encode("utf-8"))
+
+                self.get_logger().info('Sent message: %s' % message)
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    serial_interface = SerialInterface()
+
+    rclpy.spin(serial_interface)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    serial_interface.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
